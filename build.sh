7efca512b1669d644e04d3723739b7e020f99e17
sironasgor <sironasgor@gmail.com>
2025-11-20 11:53:57 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# setup color
+red='\033[0;31m'
+green='\e[0;32m'
+white='\033[0m'
+yellow='\033[0;33m'
+
+WORK_DIR=$(pwd)
+KERN_IMG="${WORK_DIR}/out/arch/arm64/boot/Image-gz.dtb"
+KERN_IMG2="${WORK_DIR}/out/arch/arm64/boot/Image.gz"
+
+function build_kernel() {
+    echo -e "\n"
+    echo -e "$yellow << building kernel >> \n$white"
+    echo -e "\n"
+    
+    rm -rf out
+    
+    make -j3 O=out ARCH=arm64 rolex_defconfig
+    make -j3 ARCH=arm64 O=out \
+                          CROSS_COMPILE=aarch64-linux-gnu- \
+                          CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                          CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
+                          
+    if [ -e "$KERN_IMG" ] || [ -e "$KERN_IMG2" ]; then
+        echo -e "\n"
+        echo -e "$green << compile kernel success! >> \n$white"
+        echo -e "\n"
+    else
+        echo -e "\n"
+        echo -e "$red << compile kernel failed! >> \n$white"
+        echo -e "\n"
+    fi
+}
+
+# execute
+build_kernel
